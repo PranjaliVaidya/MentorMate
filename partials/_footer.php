f8048aebebdb9145168d9782796993b197a7bc99
Pranjali Vaidya <91623757+PranjaliVaidya@users.noreply.github.com>
2024-01-21 17:17:33 +0530

    footer

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,3 @@
 <div class="container-fluid bg-dark text-light">
-    <p class="text-center py=3 mb=0">copyright MentorMate-forums 2022 | All right reserved</p>
+    <p class="text-center py=3 mb=0">copyright MentorMate-forums 2024 | All right reserved</p>
 </div>
