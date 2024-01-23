d9cf914b3a803116021c4550d1b2b1147797c2ea
Pranjali Vaidya <91623757+PranjaliVaidya@users.noreply.github.com>
2024-01-23 16:08:51 +0530

    Footer Modified

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,3 @@
 <div class="container-fluid bg-dark text-light">
-    <p class="text-center py=3 mb=0">copyright MentorMate-forums 2024 | All right reserved</p>
+    <p class="text-center py=3 mb=0">copyright MentorMate-forums 2024 |<h5>Made with 💜 by my team</h5> All right reserved</p>
 </div>
